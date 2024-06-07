e17f9b79629b96183224f2d07e2b4a43f620ad53
pablobeckg <beckgarreaud@gmail.com>
2024-06-07 16:15:39 +0200

    PAblo2

--- src/modules/Credit.Card.ts
+++ src/modules/Credit.Card.ts
@@ -1,25 +1,48 @@
 class CreditCard{
-    _cardNumber: string;
+    _cardNumber: string = '';
     cardHolder: string;
-    expirationDate: Date;
-    cvv: string;
+    _expirationDate: Date = new Date;
+    _cvv: string = '';
 
-    constructor(cardNumber:string, cardHolder:string, expirationDate:Date, cvv:string){
-        this._cardNumber = cardNumber;
+    constructor( cardHolder:string){
         this.cardHolder = cardHolder;
-        this.expirationDate = expirationDate;
-        this.cvv = cvv;
     }
 
     get cardNumber(){
         return this._cardNumber;
     }
 
-    set cardNumber(cardNumberInput:string){
-        if(cardNumberInput.length == 16 ){
-            this._cardNumber = cardNumberInput;
+    set cardNumber(value: string){
+        if(value.length == 16 ){
+            this._cardNumber = value;
+            console.log("yes");
         } else {
             console.log("nope");
+        }
+    }
+
+    get expirationDate() {
+        return this._expirationDate
+    }
+
+    set expirationDate(value) {
+        if (this._expirationDate < value) {
+            console.log("yessexpiration");
+        } else {
+            console.log('nopeexpiration')
+        }
+    }
+
+    get cvv() {
+        return this._cvv
+    }
+
+    set cvv(value: string) {
+        if(value.length == 3 ){
+            this._cardNumber = value;
+            console.log('yescvv')
+        } else {
+            console.log("nopecvv");
         }
     }
     
